6ab94ad6114d8c6f22441a585b7ada834402fa1d
deliang.cai <deliang.cai@monstar-lab.com>
2016-07-08 17:00:37 +0800

    进行了格式化

--- igaming/src/main/java/com/monstar/igaming/mvc/Client.java
+++ igaming/src/main/java/com/monstar/igaming/mvc/Client.java
@@ -1,5 +1,7 @@
 package com.monstar.igaming.mvc;
 
+import org.junit.Test;
+
 public class Client {
 
 	/**
@@ -33,7 +35,9 @@ public class Client {
 	 *
 	 * @param args
 	 */
-	public static void main(String[] args) {
+
+	@Test
+	public void compareStatic() {
 
 		String pname = "apple";
 
@@ -44,6 +48,77 @@ public class Client {
 		co.create(pname + 2);
 
 		co = new CO();
+		co.create(pname + 3);
+	}
+
+	/**
+	 * 
+	 * 加static,一次反射多次newInstance<BR>
+	 * com.monstar.igaming.mvc.VO<BR>
+	 * com.monstar.igaming.mvc.MO<BR>
+	 * com.monstar.igaming.mvc.CO<BR>
+	 * com.monstar.igaming.mvc.CO = apple1<BR>
+	 * com.monstar.igaming.mvc.CO<BR>
+	 * com.monstar.igaming.mvc.CO = apple2<BR>
+	 * com.monstar.igaming.mvc.CO<BR>
+	 * com.monstar.igaming.mvc.CO = apple3<BR>
+	 * 
+	 * @throws ClassNotFoundException
+	 * @throws InstantiationException
+	 * @throws IllegalAccessException
+	 */
+	@SuppressWarnings("unchecked")
+	@Test
+	public void testRef() throws ClassNotFoundException,
+			InstantiationException, IllegalAccessException {
+		String pname = "apple";
+		String clazzPath = "com.monstar.igaming.mvc.CO";
+
+		Class<CO> clazz = (Class<CO>) Class.forName(clazzPath); // 使用反射的形式
+
+		CO co = clazz.newInstance();
+		co.create(pname + 1);
+
+		co = clazz.newInstance();
+		co.create(pname + 2);
+
+		co = clazz.newInstance();
+		co.create(pname + 3);
+	}
+
+	/**
+	 * 
+	 * 加static,一次反射对应一次newInstance<BR>
+	 * com.monstar.igaming.mvc.VO<BR>
+	 * com.monstar.igaming.mvc.MO<BR>
+	 * com.monstar.igaming.mvc.CO<BR>
+	 * com.monstar.igaming.mvc.CO = apple1<BR>
+	 * com.monstar.igaming.mvc.CO<BR>
+	 * com.monstar.igaming.mvc.CO = apple2<BR>
+	 * com.monstar.igaming.mvc.CO<BR>
+	 * com.monstar.igaming.mvc.CO = apple3<BR>
+	 * 
+	 * @throws ClassNotFoundException
+	 * @throws InstantiationException
+	 * @throws IllegalAccessException
+	 */
+	@SuppressWarnings("unchecked")
+	@Test
+	public void testRef2() throws ClassNotFoundException,
+			InstantiationException, IllegalAccessException {
+		String pname = "apple";
+		String clazzPath = "com.monstar.igaming.mvc.CO";
+
+		Class<CO> clazz = (Class<CO>) Class.forName(clazzPath); // 使用反射的形式
+		CO co = clazz.newInstance();
+		co.create(pname + 1);
+
+		clazz = (Class<CO>) Class.forName(clazzPath);
+		co = clazz.newInstance();
+		co.create(pname + 2);
+
+		clazz = (Class<CO>) Class.forName(clazzPath);
+		co = clazz.newInstance();
 		co.create(pname + 3);
 	}
 
