04df0a3445028a7acda734992437cd25cc0b2242
lightfight <ursobeautiful@qq.com>
2017-04-20 10:18:28 +0800

    递变过程：重叠构建器模式-->JavaBeans模式--Builder模式

--- igaming/src/main/java/org/effectivejava/examples/chapter02/item02/builder/NutritionFacts.java
+++ igaming/src/main/java/org/effectivejava/examples/chapter02/item02/builder/NutritionFacts.java
@@ -1,6 +1,13 @@
 // Builder Pattern - Pages 14-15
 package org.effectivejava.examples.chapter02.item02.builder;
 
+/**
+ * 1.类的所有属性都是final的，必须在构造方法里进行设值,这样可以保证类的不变性;
+ * 2.构造方法是private私有的
+ * 3.从结构上讲,一个类里面包含一个`public static class Builder`，Builder的构建方法传入的是必须参数，
+ * 	 非必须参数的方法名就是属性名本身，设值后返回Builder对象本身，方便再次调用
+ * 4.递变过程：重叠构建器模式-->JavaBeans模式--Builder模式
+ */
 public class NutritionFacts {
 	private final int servingSize;
 	private final int servings;
